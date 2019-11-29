61652ed0fb8b2a0dc35ff6319efa09801cec0526
Pedro Illaisaca <pillaisaca@ups.edu.ec>
2019-11-29 10:04:55 -0500

    localizacion

---
+++ private/controladores/user/mapa.js
@@ -0,0 +1,8 @@
+var map;
+var boutique_lat= -2.95611390;
+var boutique_log = -78.9843250;
+
+var directionsDisplay =new  google.maps.DirectionsRender({polylineOptions:{strokeColor:'#2E9AFE'}});
+var directionsService = new google.maps.DirectionsService();
+
+function st
